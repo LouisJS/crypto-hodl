2b7054b9f7ab8bc36065c1c0cc8b28f1064d63d2
Louis Lecocq <my.lecocq@gmail.com>
2018-01-03 14:51:59 +0100

    fix: Crypto Reducer

--- src/reducers/CryptoReducer.js
+++ src/reducers/CryptoReducer.js
@@ -17,25 +17,25 @@ export default function(state = initialState, action) {
 
   switch(action.type) {
     case FETCHING_COIN_DATA:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: true,
         data: null,
         hasError: false,
         errorMessage: null
       });
     case FETCHING_COIN_DATA_SUCCESS:
-        return Object.assign(state, {
+        return Object.assign({}, state, {
           isFetching: false,
           data: action.payload,
           hasError: false,
           errorMessage: null
         });
     case FETCHING_COIN_DATA_FAIL:
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         isFetching: false,
-        data: null,
+        data: action.payload,
         hasError: true,
-        errorMessage: action.payload
+        errorMessage: action.err
       });
     default:
       return state;
